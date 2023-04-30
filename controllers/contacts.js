15096c14e250bd0bb74db590fe7a4b662667f6fd
Skyler Simpson <sjskyler27@gmail.com>
2023-04-30 10:40:42 -0600

    one liner

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -10,9 +10,9 @@ const getAll = async (req, res) => {
 
   const get = async (req, res) => {
     const db = await database.connectDatabase();
-    const id = req.params.id;
-    const query = { _id: id};
-    const result = await db.collection('contacts').findOne(query);
+    // const id = req.params.id;
+    // const query = { _id: id};
+    const result = await db.collection('contacts').findOne({ _id: new ObjectId(req.params.id) });
     console.log(result);
     res.send(result);
   };
