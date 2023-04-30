b4f324ecbc9c568520d682cc42dc43fd4651db8b
Skyler Simpson <sjskyler27@gmail.com>
2023-04-30 10:55:34 -0600

    change find one to find

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -2,20 +2,20 @@ const database = require('../db/connect.js');
 const { ObjectId } = require('mongodb'); // this helps with getting the id
 
 const getAll = async (req, res) => {
-    const db = await database.connectDatabase();
-    const result = await db.collection('contacts').find().toArray();
-    console.log(result);
-    res.send(result);
-  };
+  const db = await database.connectDatabase();
+  const result = await db.collection('contacts').find().toArray();
+  console.log(result);
+  res.send(result);
+};
 
-  const get = async (req, res) => {
-    const db = await database.connectDatabase();
-    // const id = req.params.id;
-    // const query = { _id: id};
-    const result = await db.collection('contacts').findOne({ _id: new ObjectId(req.params.id) });
-    console.log(result);
-    res.send(result);
-  };
+const get = async (req, res) => {
+  const db = await database.connectDatabase();
+  // const id = req.params.id;
+  // const query = { _id: id};
+  const result = await db.collection('contacts').find({ _id: new ObjectId(req.params.id) });
+  console.log(result);
+  res.send(result);
+};
 
 module.exports = {
     get,
